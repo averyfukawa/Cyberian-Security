dadf4eff137b7cafb13428c0c4132c54982c1675
Bjarne ten Bolscher <466348@student.saxion.nl>
2021-02-18 15:57:58 +0100

    [Add] a model class for answers

---
+++ Proof of concept/Assets/Scripts/FraudMessage/Answer.cs
@@ -0,0 +1,29 @@
+﻿namespace FraudMessage
+{
+    public class Answer
+    {
+        private static int _id = 0;
+        private int _answerId;
+        private int _nextScenario;
+        private string _answerText;
+        
+        public Answer(int _nextScenario, string _answerText)
+        {
+            _answerId = _id;
+            this._nextScenario = _nextScenario;
+            this._answerText = _answerText;
+
+            _id++;
+        }
+
+        public int Get_nextScenario()
+        {
+            return this._nextScenario;
+        }
+        
+        public string Get_answerText()
+        {
+            return this._answerText;
+        }
+    }
+}
