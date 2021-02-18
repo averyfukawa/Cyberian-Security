ddcf85d142b9407891d4218bda04a843231f7915
Julien Ruumpol <455502@student.saxion.nl>
2021-02-18 10:07:08 +0100

    [add] Text inspector value saving

--- Proof of concept/Assets/Editor/CanvasEditor.cs
+++ Proof of concept/Assets/Editor/CanvasEditor.cs
@@ -5,16 +5,18 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
-
+[System.Serializable]
 [CustomEditor(typeof(Canvas))]
+
 public class CanvasEditor : Editor
 {
     private Canvas _tc;
     private GameObject _tcObject;
     private Transform _childs;
     private TextCreator _textCreator;
-    private string _textField;
-    private string _trueTextField;
+
+    [SerializeField] private string _textField;
+    [SerializeField] private string _trueTextField;
 
 
     private void OnEnable()
@@ -23,7 +25,10 @@ public class CanvasEditor : Editor
         _tcObject = _tc.gameObject;
         _childs = _tcObject.gameObject.GetComponentInChildren<Transform>();
 
-
+        _textField = GetPref("TextField");
+        _trueTextField = GetPref("TrueText");
+            
+        // find the sentencebutton in order to set the text. 
         foreach (Transform t in _childs)
         {
             if (t.name == "SentenceButton")
@@ -36,12 +41,12 @@ public class CanvasEditor : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-
-
+        
         GUILayout.BeginHorizontal();
         GUILayout.Label("textField");
         _textField = GUILayout.TextArea(_textField);
         GUILayout.EndHorizontal();
+        
         GUILayout.BeginHorizontal();
         GUILayout.Label("TrueTextField");
         _trueTextField = GUILayout.TextArea(_trueTextField);
@@ -49,10 +54,33 @@ public class CanvasEditor : Editor
 
         if (GUILayout.Button("Set text"))
         {
-            
             _textCreator.textfield = _textField;
             _textCreator.trueTextField = _trueTextField;
+            SetPrefs();
         }
     }
-    
+
+    private void OnDestroy()
+    {
+        EditorPrefs.SetString("TrueText", _trueTextField);
+        EditorPrefs.SetString("TextField", _textField);
+    }
+
+    private String GetPref(string key)
+    {
+        string value = "";
+        if (EditorPrefs.HasKey(key))
+        {
+            value = EditorPrefs.GetString(key);
+        }
+
+        return value;
+    }
+
+    private void SetPrefs()
+    {
+        EditorPrefs.SetString("TrueText", _trueTextField);
+        EditorPrefs.SetString("TextField", _textField);
+
+    }
 }
