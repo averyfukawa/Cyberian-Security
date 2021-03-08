658e0a4acfc3257f5f6d301af155be02885fdb46
BjarneTenBolscher <466348@student.saxion.nl>
2021-03-08 16:03:57 +0100

    [ADD] Made the basic website functionality. Looks like shit but I am not a artist

---
+++ Liquidwire/Assets/Scripts/Games/WebsiteDiscrepancy/Selectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    public bool isScam = false;
+    private bool _isSelected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSelected()
+    {
+        _isSelected = !_isSelected;
+    }
+    
+    public bool CheckAnswer()
+    {
+        return isScam == _isSelected;
+    }
+}
