00c9d52c75da41be8c6c240c09c3f4c1e9328671
Sven <Sven.Sackmann@web.de>
2021-03-10 14:27:37 +0100

    [Add] a frankly magical solution that still looks horrid
    render texture swaps with screenspace UI, also extension for easier recttransform

--- Liquidwire/Assets/Scripts/UI/VirtualScreenSpaceCanvaser.cs
+++ Liquidwire/Assets/Scripts/UI/VirtualScreenSpaceCanvaser.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 
 public class VirtualScreenSpaceCanvaser : MonoBehaviour
 {
-    [SerializeField] private GameObject _canvas;
+    [SerializeField] private Canvas _canvas;
     [SerializeField] private GameObject _placeHolderImage;
+    [SerializeField] private Camera _virtualCamera;
+    [SerializeField] private Transform _virtualRoot;
+    [SerializeField] private Transform _trueRoot;
+    [SerializeField] private Transform _window;
+    private RectTransform _windowRect;
+    private Camera _mainCamera;
+    
 
     private void Start()
     {
-        _canvas.SetActive(false);
+        _mainCamera = Camera.main;
+        _windowRect = _window.GetComponent<RectTransform>();
+        _canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        _canvas.worldCamera = _virtualCamera;
     }
 
     public void ToggleCanvas()
     {
-        _canvas.SetActive(!_canvas.activeSelf);
+        if (_canvas.worldCamera.Equals(_virtualCamera))
+        {
+            _canvas.worldCamera = _mainCamera;
+            _canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            _window.SetParent(_trueRoot, false);
+            _windowRect.SetAll(0);
+        }
+        else
+        {
+            _canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            _canvas.worldCamera = _virtualCamera;
+            _window.SetParent(_virtualRoot, false);
+            _windowRect.SetAll(0);
+        }
         _placeHolderImage.SetActive(!_placeHolderImage.activeSelf);
     }
 }
