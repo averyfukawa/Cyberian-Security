bbd6a003af6b0e84836d6e3cb3dcdaa8df9e42a5
Sven <Sven.Sackmann@web.de>
2021-03-10 12:20:49 +0100

    [Add] Hacky solution to problem, waiting for dennis

---
+++ Liquidwire/Assets/Scripts/UI/VirtualScreenSpaceCanvaser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualScreenSpaceCanvaser : MonoBehaviour
+{
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _placeHolderImage;
+
+    private void Start()
+    {
+        _canvas.SetActive(false);
+    }
+
+    public void ToggleCanvas()
+    {
+        _canvas.SetActive(!_canvas.activeSelf);
+        _placeHolderImage.SetActive(!_placeHolderImage.activeSelf);
+    }
+}
