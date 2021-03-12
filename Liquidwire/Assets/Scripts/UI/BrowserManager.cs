da6723fa961ce0ed88713320f68aa9898ab0568f
Sven <Sven.Sackmann@web.de>
2021-03-12 14:18:04 +0100

    [Add] all of the browser tab functionality

---
+++ Liquidwire/Assets/Scripts/UI/BrowserManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BrowserManager : MonoBehaviour
+{
+    public static BrowserManager Instance;
+    [SerializeField] private Transform _untabOverlay;
+    [SerializeField] private Transform _adressBarTrans;
+    [SerializeField] private TextMeshProUGUI _adressBar;
+    [SerializeField] private GameObject _tabPrefab;
+    [SerializeField] private GameObject _tabSecureIcon;
+    private List<Tab> _tabList = new List<Tab>();
+    private Tab _activeTab;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void CloseTab(Tab tabToClose)
+    {
+        bool afterClosed = false;
+        for (int i = 0; i < _tabList.Count; i++)
+        {
+            if (_tabList[i].Equals(tabToClose))
+            {
+                afterClosed = true;
+                if (tabToClose.Equals(_activeTab))
+                {
+                    SetActiveTab(_tabList[i-1]);
+                }
+            }
+            else if(afterClosed)
+            {
+                _tabList[i].IndentHead(i-1);
+            }
+        }
+        _tabList.Remove(tabToClose);
+        Destroy(tabToClose.gameObject);
+        // TODO add additional functionality for half finished cases here
+    }
+
+    public void NewTestTab()
+    {
+        NewTab(new TabInfo("Testing Tab", "HTTPS://www.testing.tab.com/testTab", true));
+    }
+
+    public void NewTab(TabInfo newTabInfo)
+    {
+        if (_tabList.Count < 5)
+        {
+            Tab newTab = Instantiate(_tabPrefab, transform).GetComponent<Tab>();
+            newTab.IndentHead(_tabList.Count);
+            _tabList.Add(newTab);
+            newTab.SetInfo(newTabInfo);
+            SetActiveTab(newTab);
+        }
+        else
+        {
+            Debug.Log("Solve some cases first, you swine !");
+        }
+    }
+
+    public void SetActiveTab(Tab newActiveTab)
+    {
+        if (_activeTab != null)
+        {
+            _activeTab.tabBody.SetActive(false);
+        }
+        newActiveTab.tabBody.SetActive(true);
+        _untabOverlay.SetAsLastSibling();
+        newActiveTab.transform.SetAsLastSibling();
+        _adressBarTrans.SetAsLastSibling();
+        _adressBar.text = newActiveTab.tabURL;
+        _tabSecureIcon.SetActive(newActiveTab.isSecure);
+        _activeTab = newActiveTab;
+    }
+}
---
+++ Liquidwire/Assets/Scripts/UI/Tab.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Tab : MonoBehaviour
+{
+    public string tabURL;
+    public bool isSecure;
+    public GameObject tabBody;
+    [SerializeField] private TextMeshProUGUI _tabHeadText;
+    [SerializeField] private RectTransform _tabHeadTrans;
+    [SerializeField] private Vector3 _tabHeadBaseOffset;
+
+    public void IndentHead(int tabIndex)
+    {
+        _tabHeadTrans.LeanMove(_tabHeadBaseOffset + new Vector3(tabIndex * _tabHeadTrans.rect.width, 0, 0), 0.5f);
+    }
+    
+    public void SetInfo(TabInfo info)
+    {
+        _tabHeadText.text = info.tabHeadText;
+        tabURL = info.tabURL;
+        isSecure = info.isSecure;
+    }
+
+    public void SetActive()
+    {
+        BrowserManager.Instance.SetActiveTab(this);
+    }
+
+    public void Close()
+    {
+        BrowserManager.Instance.CloseTab(this);
+    }
+}
+
+[Serializable]
+public class TabInfo
+{
+    // used to accurately fill the fields of the browser
+    public string tabHeadText;
+    public string tabURL;
+    public bool isSecure;
+
+    public TabInfo(string tabHead, string tabUrl, bool isSecur)
+    {
+        tabHeadText = tabHead;
+        tabURL = tabUrl;
+        isSecure = isSecur;
+    }
+}
