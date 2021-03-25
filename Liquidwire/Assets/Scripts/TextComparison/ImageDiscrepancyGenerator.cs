afb5582c32765d510fdb5965c000f04b5ba25abf
JulienRuumpol <47635256+JulienRuumpol@users.noreply.github.com>
2021-03-25 11:12:47 +0100

    [change] abn image now split into 2 images
    Splitted the abn abn image into 2 images so that it allows for tigther control of image changing
    
    Co-Authored-By: BjarneTenBolscher <47557665+BjarneTenBolscher@users.noreply.github.com>

--- Liquidwire/Assets/Scripts/TextComparison/ImageDiscrepancyGenerator.cs
+++ Liquidwire/Assets/Scripts/TextComparison/ImageDiscrepancyGenerator.cs
@@ -14,10 +14,22 @@ namespace TextComparison
         // reference to fontAssest
         public List<TMP_FontAsset> list;
         private Vector2 _original;
-
+        private Vector2 _originalSplint;
+        [SerializeField] private GameObject _imageChild;
         public void Start()
         {
             _original = gameObject.GetComponent<RectTransform>().sizeDelta;
+            _originalSplint = _imageChild.GetComponent<RectTransform>().sizeDelta;
+            Color newColor;
+            if (ColorUtility.TryParseHtmlString("#008f8b", out newColor))
+            {
+                gameObject.GetComponent<Image>().color = newColor;
+            }
+            Color newColor2;
+            if (ColorUtility.TryParseHtmlString("#f2c100", out newColor2))
+            {
+                _imageChild.GetComponent<Image>().color = newColor2;
+            }
         }
 
         public void GenerateDiscrapency(int difficulty)
@@ -25,15 +37,14 @@ namespace TextComparison
             int rng = Random.Range(0, 100);
             TextMeshProUGUI child = gameObject.GetComponentInChildren<TextMeshProUGUI>();
             bool changed = false;
-
-
+            
             if (rng > (difficulty * 4))
             {
                 child.fontSize = 50;
                 gameObject.GetComponent<ImageDiscrepancy>().isScam = true;
                 changed = true;
             }
-
+            
             if (rng > (difficulty * 7))
             {
                 int index = Random.Range(0, list.Count);
@@ -41,28 +52,41 @@ namespace TextComparison
                 gameObject.GetComponent<ImageDiscrepancy>().isScam = true;
                 changed = true;
             }
-
+            
             if (rng > (difficulty * 9))
             {
                 int size = Random.Range(70, 130);
                 gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
+                _imageChild.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
                 changed = true;
             }
-
             if (rng > (difficulty * 2))
             {
-                //TODO: Change the obnoxiously bright colors
-                var temp = gameObject.GetComponent<RawImage>().color;
-                temp.a = 1;
+                gameObject.GetComponentInChildren<Image>().color = Color.blue;
+                gameObject.GetComponent<Image>().color = Color.yellow;
+                gameObject.GetComponent<ImageDiscrepancy>().isScam = true;
             }
-
             if (!changed)
             {
                 gameObject.GetComponent<ImageDiscrepancy>().isScam = false;
                 gameObject.GetComponent<RectTransform>().sizeDelta = _original;
+                _imageChild.GetComponent<RectTransform>().sizeDelta = _originalSplint;
+                
+                Color newColor;
+                if (ColorUtility.TryParseHtmlString("#008f8b", out newColor))
+                {
+                    gameObject.GetComponent<Image>().color = newColor;
+                }
+                Color newColor2;
+                if (ColorUtility.TryParseHtmlString("#f2c100", out newColor2))
+                {
+                    _imageChild.GetComponent<Image>().color = newColor2;
+                }
+                
                 child.font = list[0];
                 child.fontSize = 36;
             }
         }
+        
     }
 }
