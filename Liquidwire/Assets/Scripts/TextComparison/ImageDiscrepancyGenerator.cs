808b8ae8c8a58bf621ab959e21771a3ff330f4c4
JulienRuumpol <47635256+JulienRuumpol@users.noreply.github.com>
2021-03-26 14:34:42 +0100

    [change]
    Added textfield boolean to check if you want text based changes.
    Removed changeImageSize.
    
    Co-Authored-By: BjarneTenBolscher <47557665+BjarneTenBolscher@users.noreply.github.com>

--- Liquidwire/Assets/Scripts/TextComparison/ImageDiscrepancyGenerator.cs
+++ Liquidwire/Assets/Scripts/TextComparison/ImageDiscrepancyGenerator.cs
@@ -17,53 +17,51 @@ namespace TextComparison
         private Vector2 _originalSplint;
         private Color _originalPieceColor;
         private Color _originalColor;
-        [Range(0, 1)]
-        [SerializeField] private float minDifference;
-        [Range(0, 1)]
-        [SerializeField] private float maxDifference;
-        
+        [Range(0, 1)] [SerializeField] private float minDifference;
+        [Range(0, 1)] [SerializeField] private float maxDifference;
+
         [SerializeField] private GameObject _imageChild;
         private TextMeshProUGUI _child;
         private bool _changed;
+        [SerializeField] private bool _textChild = false;
 
         public void Start()
         {
             _original = gameObject.GetComponent<RectTransform>().sizeDelta;
             _originalSplint = _imageChild.GetComponent<RectTransform>().sizeDelta;
-            
+
             ColorUtility.TryParseHtmlString("#f2c100", out _originalPieceColor);
             ColorUtility.TryParseHtmlString("#008f8b", out _originalColor);
-            
+
             _imageChild.GetComponent<Image>().color = _originalPieceColor;
             gameObject.GetComponent<Image>().color = _originalColor;
+
+            if (gameObject.GetComponentInChildren<TextMeshProUGUI>() != null)
+            {
+                _child = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+                _textChild = true;
+            }
         }
 
         public void GenerateDiscrapency(int difficulty)
         {
+            ResetAll();
             int rng = Random.Range(0, 100);
-            _child = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
             _changed = false;
-            if (rng > (difficulty * 4))
-            {
-                ChangeFontSize();
-            }
-            
-            if (rng > (difficulty * 7))
+            if (rng > (difficulty * 7) && _textChild)
             {
                 ChangeFont();
             }
-            
-            if (rng > (difficulty * 9))
+            else if (rng > (difficulty * 4) && _textChild)
             {
-                ChangeSize();
+                ChangeFontSize();
             }
-            
-            if (rng > (difficulty * 2))
+            else if (rng > (difficulty * 2))
             {
                 ChangeColor();
             }
-            
-            if (!_changed)
+            else
             {
                 ResetAll();
             }
@@ -71,7 +69,8 @@ namespace TextComparison
 
         private void ChangeFontSize()
         {
-            _child.fontSize = 50;
+            int index = Random.Range(20, 40);
+            _child.fontSize = index;
             gameObject.GetComponent<ImageDiscrepancy>().isScam = true;
             _changed = true;
         }
@@ -84,26 +83,17 @@ namespace TextComparison
             _changed = true;
         }
 
-        private void ChangeSize()
-        {
-            int size = Random.Range(70, 130);
-            gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
-            _imageChild.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
-            _changed = true;
-        }
-
         private void ChangeColor()
         {
-            float index = Random.Range(minDifference, maxDifference);
+            _imageChild.GetComponent<Image>().color = IncrementColor(_originalPieceColor);
+            gameObject.GetComponent<Image>().color = IncrementColor(_originalColor);
 
-            _imageChild.GetComponent<Image>().color = IncrementColor(_originalPieceColor, index);
-            gameObject.GetComponent<Image>().color = IncrementColor(_originalColor, index);
-            
             gameObject.GetComponent<ImageDiscrepancy>().isScam = true;
         }
 
-        private static Color IncrementColor(Color color, float index)
+        private Color IncrementColor(Color color)
         {
+            float index = Random.Range(minDifference, maxDifference);
             color.b -= index;
             color.g -= index;
             color.r -= index;
@@ -118,7 +108,7 @@ namespace TextComparison
 
             _imageChild.GetComponent<Image>().color = _originalPieceColor;
             gameObject.GetComponent<Image>().color = _originalColor;
-            
+
             _child.font = list[0];
             _child.fontSize = 36;
         }
