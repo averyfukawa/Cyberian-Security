db2074efaf999e6eb4d9050fe171fd3161c408d3
Avery <averyfukawa@gmail.com>
2021-03-30 12:33:15 +0200

    [ADD] 3D audio script experimentation

---
+++ Liquidwire/Assets/Scripts/Audio/Play3DSound.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play3DSound : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string selectSound;
+    private FMOD.Studio.EventInstance soundEvent;
+
+    public KeyCode pressToPlaySound;
+
+    private void Start()
+    {
+        soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectSound);
+    }
+
+    private void Update()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        PlaySound();
+    }
+
+    private void PlaySound()
+    {
+        if (Input.GetKey(pressToPlaySound))
+        {
+            FMOD.Studio.PLAYBACK_STATE fmodPbState;
+            soundEvent.getPlaybackState(out fmodPbState);
+            if (fmodPbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                soundEvent.start();
+            }
+        }
+
+        if (Input.GetKeyUp(pressToPlaySound))
+        {
+            soundEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+    }
+}
