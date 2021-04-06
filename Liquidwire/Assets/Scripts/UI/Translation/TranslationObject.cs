89863b8bd4d13e5040aea4a1a220206e9d1fd69f
Bjarne ten Bolscher <466348@student.saxion.nl>
2021-04-06 11:46:08 +0200

    [Add] Translation functionality
    When you check the translation editor it will show you which textFields haven't been filled in yet.

---
+++ Liquidwire/Assets/Scripts/UI/Translation/TranslationObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class TranslationObject
+{
+    public LanguageScript.Languages language;
+    public string translation;
+
+    public TranslationObject(string translation, LanguageScript.Languages language)
+    {
+        this.language = language;
+        this.translation = translation;
+    }
+}
---
+++ Liquidwire/Assets/Scripts/UI/Translation/TranslationScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TranslationScript : MonoBehaviour
+{
+    [SerializeField] private List<TranslationObject> _translation;
+
+    private LanguageScript _languageScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetLanguage();
+    }
+    public void SetLanguage()
+    {
+        _languageScript = FindObjectOfType<LanguageScript>();
+        foreach (var item in _translation)
+        {
+            if (item.language == _languageScript.languages)
+            {
+                gameObject.GetComponent<TextMeshProUGUI>().text = item.translation;
+            }
+        }
+    }
+
+    public List<TranslationObject> GetList()
+    {
+        return _translation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
