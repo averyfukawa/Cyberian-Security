78fc433eb6a7720ac4fd6371fffdda3ae1fc7f54
Avery <averyfukawa@gmail.com>
2021-04-21 10:30:58 +0200

    [ADD] Working Volume slider prefab

---
+++ Liquidwire/Assets/Scripts/Audio/VCAController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMOD.Studio;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VCAController : MonoBehaviour
+{
+    private FMOD.Studio.VCA vcaControl;
+    public string vcaName;
+
+    [SerializeField] private float vcaVolume;
+    
+    private Slider volumeSlider;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        vcaControl = FMODUnity.RuntimeManager.GetVCA("vca:/" + vcaName);
+        volumeSlider = GetComponent<Slider>();
+
+        vcaControl.getVolume(out vcaVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        vcaControl.setVolume(volume);
+        
+        vcaControl.getVolume(out vcaVolume);
+    }
+}
