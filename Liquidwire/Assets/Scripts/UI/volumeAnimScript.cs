63a9bd9d9c677955333fd0dbbdf393d06867407a
Avery <averyfukawa@gmail.com>
2021-04-22 10:40:14 +0200

    [ADD] Slide animations

---
+++ Liquidwire/Assets/Scripts/UI/volumeAnimScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volumeAnimScript : MonoBehaviour
+{
+    [SerializeField] private Animation volumeAnim;
+
+    public void PlayOpenAnim()
+    {
+        volumeAnim.Play("anim_volume_slide");
+    }
+
+    public void PlayCloseAnim()
+    {
+        volumeAnim.Play("anim_volume_slideBackwards");
+    }
+}
