44ae209b0f27e98d998508fddc0ad8bd7d4b96f7
Sven <Sven.Sackmann@web.de>
2021-04-26 12:36:23 +0200

    [Add] email scrolling (still not fully bug free)

---
+++ Liquidwire/Assets/Scripts/UI/Browser/WebsiteScroller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WebsiteScroller : MonoBehaviour
+{
+    [SerializeField] private bool _isBasicVariant = true;
+    [SerializeField] private TextMeshProUGUI _referenceLine;
+    [SerializeField] private TextMeshProUGUI _bodyText;
+
+    private void Start()
+    {
+        if (_isBasicVariant && _referenceLine != null && _bodyText!=null)
+        {
+            _bodyText.fontSize = _referenceLine.fontSize;
+        }
+    }
+}
