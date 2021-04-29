648118106d8aaa3da1ecab2419e6ea157108a969
Bjarne ten Bolscher <466348@student.saxion.nl>
2021-04-29 15:04:42 +0200

    [Change] File location and added regions

---
+++ Liquidwire/Assets/Scripts/UI/Browser/Emails/EmailInbox.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Player.Save_scripts.Artificial_dictionaries;
+using UnityEngine;
+
+namespace UI.Browser.Emails
+{
+    public class EmailInbox : MonoBehaviour
+    {
+        [SerializeField] private Transform _inboxTrans;
+        [SerializeField] private List<EmailListing> _currentEmails = new List<EmailListing>();
+        private int _currentCaseNumber;
+
+        public void NewEmail(GameObject newListingPrefab)
+        {
+            GameObject newEmail = Instantiate(newListingPrefab, _inboxTrans);
+            EmailListing newListing = newEmail.GetComponent<EmailListing>();
+            _currentEmails.Add(newListing);
+            _currentCaseNumber++;
+            newListing.caseNumber = _currentCaseNumber;
+            newListing.SetVisuals();
+            RectTransform newMailRect = newEmail.GetComponent<RectTransform>();
+            newMailRect.Translate(new Vector3(0, (newMailRect.rect.height) * -(_currentEmails.Count - 1), 0));
+        }
+
+
+        public void LoadEmail(GameObject newListingPrefab, EmailListingPosition position, int status)
+        {
+            GameObject newEmail = Instantiate(newListingPrefab, _inboxTrans);
+            EmailListing newListing = newEmail.GetComponent<EmailListing>();
+            newListing.currentStatus = (EmailListing.CaseStatus) status;
+            _currentEmails.Add(newListing);
+            _currentCaseNumber++;
+            newListing.caseNumber = _currentCaseNumber;
+            newListing.SetVisuals();
+            RectTransform newMailRect = newEmail.GetComponent<RectTransform>();
+            newMailRect.offsetMax = new Vector2(newMailRect.offsetMax.x, position.GetOffsetMaxY());
+            newMailRect.offsetMin = new Vector2(newMailRect.offsetMin.x, position.GetOffsetMinY());
+        }
+    
+        public List<EmailListing> GetEmails()
+        {
+            return _currentEmails;
+        }
+
+        public void Reset()
+        {
+            _currentEmails = new List<EmailListing>();
+        }
+    }
+}
---
+++ Liquidwire/Assets/Scripts/UI/Browser/Tabs/WebLinkText.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UI.Browser.Tabs;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = System.Random;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class WebLinkText : MonoBehaviour, IPointerClickHandler
+{
+    private TextMeshProUGUI _sourceText;
+    private Coroutine _visualizationInstance;
+    public TabInfo[] linkedTabsByLinkID;
+    public Tab[] currentlyLinkedTabs;
+
+    public void Start()
+    {
+        _sourceText = GetComponent<TextMeshProUGUI>();
+        _visualizationInstance = StartCoroutine(WaitThenVisualize());
+        Tab thisTab = GetComponentInParent<Tab>();
+        foreach (var tab in linkedTabsByLinkID)
+        {
+            if (tab.tabHeadText == "")
+            {
+                tab.tabHeadText = "Case - " + thisTab.caseNumber;
+            }
+
+            if (tab.tabURL == "attachment")
+            {
+                tab.tabURL = thisTab.tabURL + "/attachment/" + RandomUrl(6);
+            }
+        }
+    }
+
+    #region Wait methods
+
+    private IEnumerator WaitThenVisualize()
+    {
+        yield return new WaitForEndOfFrame();
+        SetupLinkVisuals();
+        currentlyLinkedTabs = new Tab[_sourceText.textInfo.linkCount];
+    }
+    private IEnumerator WaitThenRemoveLinks()
+    {
+        yield return new WaitForEndOfFrame();
+        TMP_LinkInfo[] links = _sourceText.textInfo.linkInfo; // this will nullpointer error a lot, but afaik it is impossible to check for without extending TMP
+        _sourceText.text = _sourceText.text.Replace("</link>", "");
+        foreach (var link in links)
+        {
+            _sourceText.text = _sourceText.text.Replace("<link=" + link.GetLinkID() +">", "");
+        }
+    }
+
+    #endregion
+    
+    #region Link methods
+
+    public void RemoveLinksForPrint()
+    {
+        if (_visualizationInstance != null)
+        {
+            StopCoroutine(_visualizationInstance);
+        }
+        StartCoroutine(WaitThenRemoveLinks());
+    }
+
+    private void SetupLinkVisuals()
+    {
+        TMP_LinkInfo[] links = _sourceText.textInfo.linkInfo;
+        string _linkStartReplace = "<u color=#0000EE><color=#0000EE>";
+        string _linkEndReplace = "</u></color>";
+        _sourceText.text = _sourceText.text.Replace(_linkStartReplace, "");
+        _sourceText.text = _sourceText.text.Replace(_linkEndReplace, "");
+        for (int i = 0; i < links.Length; i++) {
+            //Append behind </link>
+            _sourceText.text = _sourceText.text.Insert(links[i].linkTextfirstCharacterIndex + links[i].linkTextLength + links[i].linkIdLength + 7, _linkEndReplace);
+            //Place before <link
+            _sourceText.text = _sourceText.text.Insert(links[i].linkTextfirstCharacterIndex, _linkStartReplace);
+        }
+    }
+
+    #endregion
+    
+    private string RandomUrl(int length)
+    {
+        string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";  
+        Random random = new Random();  
+        char[] chars = new char[length];  
+        for (int i = 0; i < length; i++)  
+        {  
+            chars[i] = validChars[random.Next(0, validChars.Length)];  
+        }  
+        return new string(chars); 
+    }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Check if the left mouse button was used to click
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            //Finds the correct link based on the mouse position and the text field
+            int linkId = TMP_TextUtilities.FindIntersectingLink(_sourceText, Input.mousePosition, null);
+            if (linkId >= 0)
+            {
+                if (currentlyLinkedTabs[linkId] != null)
+                    // check if it has a linked tab and switch to it, if it has
+                {
+                    BrowserManager.Instance.SetActiveTab(currentlyLinkedTabs[linkId]);
+                }
+                else
+                {
+                    // if not, make a new one based on its info and current state, and link it
+                    linkedTabsByLinkID[linkId].caseNumber = BrowserManager.Instance.activeTab.caseNumber;
+                    currentlyLinkedTabs[linkId] = BrowserManager.Instance.NewTab(linkedTabsByLinkID[linkId], 0);
+                }
+            }
+           
+        }
+    }
+}
